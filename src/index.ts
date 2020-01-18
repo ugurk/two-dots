b687c8572b2ac4d09a719e3026310c9976bc287c
Ugur Kocak <uk.ugur@gmail.com>
2020-01-18 22:54:04 +0300

    adds index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from './core/Dimension';
+export * from './core/Point';
+export * from './core/Serializable';
+export * from './core/Transform';
+
+export * from './elements/AbstractElement';
+
+export * from './TwoDots';
